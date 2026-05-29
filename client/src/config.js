952acb18c8749f08a525ab0d2c036b9643fbc809
kunafina-ruslana <kunafina.rr@gmail.com>
2026-05-29 10:09:18 +0300

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,13 @@
-export const API_URL = process.env.NODE_ENV === 'production' 
-  ? ''  : 'http://localhost:5000';
+const config = {
+  development: {
+    API_URL: 'http://localhost:5000'
+  },
+  production: {
+    API_URL: ''
+  }
+};
+
+const environment = process.env.NODE_ENV || 'development';
+export const API_URL = config[environment].API_URL;
+
+export default config;
