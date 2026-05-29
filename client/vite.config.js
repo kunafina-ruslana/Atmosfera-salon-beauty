620bdce155bb09fecef70713162867f95f581471
kunafina-ruslana <kunafina.rr@gmail.com>
2026-05-29 10:54:15 +0300

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,19 +3,20 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
+server: {
     port: 3000,
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true,
-        secure: false
       },
       '/uploads': {
         target: 'http://localhost:5000',
         changeOrigin: true,
-        secure: false
-      }
-    }
-  }
-})
+      },
+    },
+  },
+  build: {
+    outDir: 'build',
+  },
+});
