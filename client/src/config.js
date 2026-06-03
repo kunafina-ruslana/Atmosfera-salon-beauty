99f7f30c6c866b9f82438ab935c87054131b456f
kunafina-ruslana <kunafina.rr@gmail.com>
2026-06-03 11:55:37 +0300

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,1 @@
-export const API_URL = process.env.NODE_ENV === 'production' 
-  ? 'https://atmosfera-salon-beauty-api.onrender.com' 
-  : 'http://localhost:5000';
+export const API_URL = 'https://salon-api.onrender.com';
