181c63c3e646ca639f1683f6030f56b9307052b3
kunafina-ruslana <kunafina.rr@gmail.com>
2026-06-04 14:00:01 +0300

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,17 +3,20 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-server: {
+  server: {
     port: 3000,
     proxy: {
       '/api': {
         target: 'https://atmosfera-salon-beauty-api.onrender.com',
         changeOrigin: true,
+        secure: true,
+        rewrite: (path) => path
       },
       '/uploads': {
         target: 'https://atmosfera-salon-beauty-api.onrender.com',
         changeOrigin: true,
-      },
+        secure: true
+      }
     },
   },
   build: {
